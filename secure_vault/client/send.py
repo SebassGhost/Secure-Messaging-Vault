aaf67e4d78a545dad282c8d1dde61afa72a15e8e
Sebastián Quintero <sq07369@gmail.com>
2026-01-23 18:37:21 -0500

    Update send.py

--- secure_vault/client/send.py
+++ secure_vault/client/send.py
@@ -2,6 +2,8 @@ from client.crypto import encrypt_and_sign
 
 
 def main():
+    print("[ Secure Messaging Vault – SEND ]\n")
+
     # Entrada interactiva del usuario
     message = input("Escribe tu mensaje: ").encode()
 
@@ -9,11 +11,17 @@ def main():
     payload = encrypt_and_sign(message)
 
     print("\n[+] Mensaje cifrado y firmado correctamente")
+    print("[*] Listo para transporte o almacenamiento seguro")
 
-    # No imprimimos secretos aquí 
-    # Se devuelve el payload para transporte / recepción
+    #  No imprimir:
+    # - key
+    # - ciphertext
+    # - signature
+    # Esto es intencional (Zero Trust)
+
     return payload
 
 
 if __name__ == "__main__":
     main()
+
