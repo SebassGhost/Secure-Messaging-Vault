d0af045113fae246d1a4f3893afb34404fab8b89
Sebastián Quintero <sq07369@gmail.com>
2026-01-23 18:20:30 -0500

    Update receive.py

--- secure_vault/client/receive.py
+++ secure_vault/client/receive.py
@@ -1,1 +1,16 @@
+from client.crypto import (
+    decrypt_message,
+    verify_signature,
+    load_public_key
+)
+
+
+def main():
+    print("[!] receive.py es un placeholder lógico")
+    print("Aquí se verificará firma, hash y se descifrará el mensaje")
+    print("cuando haya transporte real (DB o red)")
+
+
+if __name__ == "__main__":
+    main()
 
