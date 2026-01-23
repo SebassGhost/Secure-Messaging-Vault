399f3c4d4470690f812cf2efa7aef5589cee2b17
Sebastián Quintero <sq07369@gmail.com>
2026-01-23 18:19:15 -0500

    Update identity.py

--- secure_vault/client/identity.py
+++ secure_vault/client/identity.py
@@ -1,6 +1,6 @@
-from cryptography.hazmat.primitives.asymmetric import ed25519
-from cryptography.hazmat.primitives import serialization
 from pathlib import Path
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from cryptography.hazmat.primitives import serialization
 
 
 KEYS_DIR = Path("client/keys")
@@ -8,46 +8,33 @@ PRIVATE_KEY_FILE = KEYS_DIR / "private_key.pem"
 PUBLIC_KEY_FILE = KEYS_DIR / "public_key.pem"
 
 
-def generate_keys():
+def main():
     KEYS_DIR.mkdir(parents=True, exist_ok=True)
 
-    if PRIVATE_KEY_FILE.exists():
+    if PRIVATE_KEY_FILE.exists() and PUBLIC_KEY_FILE.exists():
         print("[!] Las claves ya existen.")
         return
 
-    private_key = ed25519.Ed25519PrivateKey.generate()
+    private_key = Ed25519PrivateKey.generate()
     public_key = private_key.public_key()
 
-    private_bytes = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
+    PRIVATE_KEY_FILE.write_bytes(
+        private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
     )
 
-    public_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    PUBLIC_KEY_FILE.write_bytes(
+        public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
     )
 
-    PRIVATE_KEY_FILE.write_bytes(private_bytes)
-    PUBLIC_KEY_FILE.write_bytes(public_bytes)
-
-    print("[+] Claves generadas correctamente.")
-
-
-def load_private_key():
-    return serialization.load_pem_private_key(
-        PRIVATE_KEY_FILE.read_bytes(),
-        password=None
-    )
-
-
-def load_public_key():
-    return serialization.load_pem_public_key(
-        PUBLIC_KEY_FILE.read_bytes()
-    )
+    print("[+] Identidad criptográfica creada.")
 
 
 if __name__ == "__main__":
-    generate_keys()
-
+    main()
